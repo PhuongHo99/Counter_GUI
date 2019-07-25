446e1ab8be067cca92b47a58614c9b0b8868a412
Phuong Ho <hopu@miamioh.edu>
2019-07-25 19:16:22 -0400

    First Commit. Frame of the counter and test.

---
+++ Lab08/src/Lab08Test.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author hophuong
+ *
+ */
+public class Lab08Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CFrame mainwWindow = new CFrame();
+		mainwWindow.setTitle("Counter");
+		mainwWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainwWindow.setVisible(true);
+	}
+}
